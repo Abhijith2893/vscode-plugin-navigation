070cf56ab0b3d8b2bccb98545f53b4bbcec39347
Abhijith Manohar <abhijithmanohar@Abhijiths-MacBook-Pro.local>
2021-03-23 22:05:55 -0500

    add basic navigation code

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const stack: Array<vscode.TextDocument> = [];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "file-navigator" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('file-navigator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+	let createStack = vscode.commands.registerCommand('file-navigator.createStack', () => {
 		vscode.window.showInformationMessage('Hello World from File Navigator!');
+		
+		vscode.window.onDidChangeActiveTextEditor(() => {
+			console.log(vscode.window.activeTextEditor?.document.fileName);
+			let textDocument: vscode.TextDocument = vscode.window.activeTextEditor?.document!;
+			textDocument && stack.push(textDocument);
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	let navigateBack = vscode.commands.registerCommand('file-navigator.navigateBack', () => {
+		if (stack){
+			stack.pop();
+			let currentTextDocument: vscode.TextDocument = stack.pop()!;
+			console.log("POPPED " + currentTextDocument);
+			vscode.window.showTextDocument(currentTextDocument);
+		}
+	});
+
+	context.subscriptions.push(createStack);
+	context.subscriptions.push(navigateBack);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
