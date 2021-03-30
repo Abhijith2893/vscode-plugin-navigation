0324e096c62d89db2d1e1b66c86ecbbedd2bb124
Abhijith Manohar <abhijithmanohar@Abhijiths-MacBook-Pro.local>
2021-03-29 20:42:30 -0500

    add button

--- src/extension.ts
+++ src/extension.ts
@@ -3,26 +3,21 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	const stack: Array<vscode.TextDocument> = [];
 
-	let createStack = vscode.commands.registerCommand('file-navigator.createStack', () => {
-		vscode.window.showInformationMessage('Hello World from File Navigator!');
-		
-		vscode.window.onDidChangeActiveTextEditor(() => {
-			console.log(vscode.window.activeTextEditor?.document.fileName);
-			let textDocument: vscode.TextDocument = vscode.window.activeTextEditor?.document!;
-			textDocument && stack.push(textDocument);
-		});
+	vscode.window.onDidChangeActiveTextEditor(() => {
+		let textDocument: vscode.TextDocument = vscode.window.activeTextEditor?.document!;
+		textDocument && stack.push(textDocument);
 	});
 
 	let navigateBack = vscode.commands.registerCommand('file-navigator.navigateBack', () => {
-		if (stack){
-			stack.pop();
+		if (stack.length){
+			if (stack.length !== 1){
+				stack.pop();
+			}
 			let currentTextDocument: vscode.TextDocument = stack.pop()!;
-			console.log("POPPED " + currentTextDocument);
 			vscode.window.showTextDocument(currentTextDocument);
 		}
 	});
 
-	context.subscriptions.push(createStack);
 	context.subscriptions.push(navigateBack);
 }
 
